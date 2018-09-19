bb653b756f2ae6d77b678f3f965c61ba03ebf9e5
Arjun <arjunmehta2014@gmail.com>
2018-09-19 10:03:15 -0700

    refactored Ratings component and added flexbox styles

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import $ from 'jquery';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import Search from './Search.jsx';
 import Ratings from './Ratings.jsx';
 import ReviewList from './ReviewList.jsx';
-import {
-  Container, HeaderContainer, TotalReviews, SearchBox,
-} from './styles.js'
+import { Container, HeaderContainer, TotalReviews, TotalReviewsText, HeaderRatingStars, Star, SearchBox } from './styles.js'
+
+library.add(faStar, faStarHalfAlt);
 
 export default class App extends React.Component {
+  static intToStars(rating) {
+    let stars = [...Array(Math.floor(rating))].map(() => <Star><FontAwesomeIcon icon="star" /></Star>);
+    if (rating.toString().length > 1) {
+      stars = stars.concat(<Star><FontAwesomeIcon icon="star-half-alt" /></Star>);
+    }
+    return stars;
+  }
+
   constructor() {
     super();
+    this.updateTotalAvgRating = this.updateTotalAvgRating.bind(this);
     this.state = {
       reviews: [],
+      totalAvgRating: '',
     };
   }
 
@@ -29,24 +42,38 @@ export default class App extends React.Component {
     });
   }
 
+  updateTotalAvgRating(number) {
+    this.setState({ totalAvgRating: number });
+  }
+
   render() {
     const { reviews } = this.state;
+    const { totalAvgRating } = this.state;
     return (
       <div>
         <Container>
           <HeaderContainer>
             <TotalReviews>
               <b>
-                { reviews.length }
-                {' '}
-              Reviews
+                <TotalReviewsText>
+                  { reviews.length }
+                  {' '}
+                  Reviews
+                </TotalReviewsText>
               </b>
+              <HeaderRatingStars>
+                {App.intToStars(totalAvgRating)}
+              </HeaderRatingStars>
             </TotalReviews>
             <SearchBox>
               <Search />
             </SearchBox>
           </HeaderContainer>
-          <Ratings reviews={reviews} />
+          <Ratings
+            reviews={reviews}
+            updateTotalAvgRating={this.updateTotalAvgRating}
+            intToStars={App.intToStars}
+          />
           <ReviewList reviews={reviews} />
         </Container>
       </div>
--- client/Ratings.jsx
+++ client/Ratings.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
-import { RatingsContainer, RatingsLeftContainer, RatingsRightContainer, RatingStars } from './styles.js';
-
-library.add(faStar, faStarHalfAlt);
+import { RatingsContainer, RatingsLeftContainer, RatingsRightContainer, RatingTextAndStarsContainer, RatingText, RatingStars } from './styles.js';
 
 export default class Ratings extends React.Component {
-  static intToStars(rating) {
-    let stars = [...Array(Math.floor(rating))].map(() => <RatingStars><FontAwesomeIcon icon="star" /></RatingStars>);
-    if (rating.toString().length > 1) {
-      stars = stars.concat(<RatingStars><FontAwesomeIcon icon="star-half-alt" /></RatingStars>);
-    }
-    return stars;
-  }
+  // static intToStars(rating) {
+  //   let stars = [...Array(Math.floor(rating))].map(() => <RatingStars><FontAwesomeIcon icon="star" /></RatingStars>);
+  //   if (rating.toString().length > 1) {
+  //     stars = stars.concat(<RatingStars><FontAwesomeIcon icon="star-half-alt" /></RatingStars>);
+  //   }
+  //   return stars;
+  // }
 
   constructor(props) {
     super(props);
@@ -29,42 +27,21 @@ export default class Ratings extends React.Component {
 
   componentDidUpdate(prevProps) {
     const { reviews } = this.props;
+    const { updateTotalAvgRating } = this.props;
     if (reviews !== prevProps.reviews) {
-      const roundHalf = num => Math.round(num * 2) / 2;
-      if (reviews.length > 0) {
-        let accuracySum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          accuracySum += reviews[i].accuracy_rating;
+      const roundHalf = n => Math.round(n * 2) / 2;
+      let totalAvgRating = 0;
+      const ratings = ['accuracy_rating', 'communication_rating', 'cleanliness_rating', 'location_rating', 'checkin_rating', 'value_rating'];
+      for (let i = 0; i < ratings.length; i += 1) {
+        let sum = 0;
+        for (let x = 0; x < reviews.length; x += 1) {
+          sum += reviews[x][ratings[i]];
         }
-        let communicationSum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          communicationSum += reviews[i].communication_rating;
-        }
-        let cleanlinessSum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          cleanlinessSum += reviews[i].cleanliness_rating;
-        }
-        let locationSum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          locationSum += reviews[i].location_rating;
-        }
-        let checkinSum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          checkinSum += reviews[i].checkin_rating;
-        }
-        let valueSum = 0;
-        for (let i = 0; i < reviews.length; i += 1) {
-          valueSum += reviews[i].value_rating;
-        }
-        this.setState({
-          accuracyAvg: roundHalf(accuracySum / reviews.length),
-          communicationAvg: roundHalf(communicationSum / reviews.length),
-          cleanlinessAvg: roundHalf(cleanlinessSum / reviews.length),
-          locationAvg: roundHalf(locationSum / reviews.length),
-          checkinAvg: roundHalf(checkinSum / reviews.length),
-          valueAvg: roundHalf(valueSum / reviews.length),
-        });
+        totalAvgRating += (sum / reviews.length);
+        const stateProp = Object.keys(this.state)[i];
+        this.setState({ [stateProp]: roundHalf(sum / reviews.length) });
       }
+      updateTotalAvgRating(roundHalf(totalAvgRating / 6));
     }
   }
 
@@ -73,38 +50,42 @@ export default class Ratings extends React.Component {
       accuracyAvg, communicationAvg, cleanlinessAvg,
       locationAvg, checkinAvg, valueAvg
     } = this.state;
-    const stars = Ratings.intToStars(4);
-    console.log(stars);
-
+    const { intToStars } = this.props;
     return (
       <div>
         <RatingsContainer>
           <RatingsLeftContainer>
-            Accuracy
-            {' '}
-            {Ratings.intToStars(accuracyAvg)}
-            <br />
-            Communication
-            {' '}
-            {Ratings.intToStars(communicationAvg)}
-            <br />
-            Cleanliness
-            {' '}
-            {Ratings.intToStars(cleanlinessAvg)}
-            <br />
+            <RatingTextAndStarsContainer>
+              <RatingText>Accuracy</RatingText>
+              <RatingStars>{intToStars(accuracyAvg)}</RatingStars>
+              <br />
+            </RatingTextAndStarsContainer>
+            <RatingTextAndStarsContainer>
+              <RatingText>Communication</RatingText>
+              <RatingStars>{intToStars(communicationAvg)}</RatingStars>
+              <br />
+            </RatingTextAndStarsContainer>
+            <RatingTextAndStarsContainer>
+              <RatingText>Cleanliness</RatingText>
+              <RatingStars>{intToStars(cleanlinessAvg)}</RatingStars>
+              <br />
+            </RatingTextAndStarsContainer>
           </RatingsLeftContainer>
           <RatingsRightContainer>
-            Location
-            {' '}
-            {Ratings.intToStars(locationAvg)}
-            <br />
-            Check-in
-            {' '}
-            {Ratings.intToStars(checkinAvg)}
-            <br />
-            Value
-            {' '}
-            {Ratings.intToStars(valueAvg)}
+            <RatingTextAndStarsContainer>
+              <RatingText>Location</RatingText>
+              <RatingStars>{intToStars(locationAvg)}</RatingStars>
+              <br />
+            </RatingTextAndStarsContainer>
+            <RatingTextAndStarsContainer>
+              <RatingText>Check-in</RatingText>
+              <RatingStars>{intToStars(checkinAvg)}</RatingStars>
+              <br />
+            </RatingTextAndStarsContainer>
+            <RatingTextAndStarsContainer>
+              <RatingText>Value</RatingText>
+              <RatingStars>{intToStars(valueAvg)}</RatingStars>
+            </RatingTextAndStarsContainer>
           </RatingsRightContainer>
         </RatingsContainer>
       </div>
